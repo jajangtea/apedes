9c07ae169d341b378e02f6fd6c84429a89df9a5d
jjcollection <za2ng2509@hotmail.com>
2017-10-22 12:40:32 +0700

    design add perdes dan buku Induk Penduduk

--- protected/pages/m/limitless/newEmptyPHP.php
+++ protected/pages/m/limitless/newEmptyPHP.php
@@ -1,8 +1,15 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<div class="form-group" id="divAddNoKK">
+                    <label class="col-md-2 control-label text-right">Nomor KK :</label>
+                    <div class="col-md-4">
+                        <com:TTextBox ID="txtAddNoKK" CssClass="form-control" />
+                        <com:TRequiredFieldValidator ID="FieldAddNoKKValidator" ControlToValidate="txtAddNoKK" Display="Dynamic" ErrorMessage="Isi Alamat Tempat Tinggal" ValidationGroup="editformulir" CssClass="has-error help-block">
+                            <prop:ClientSide.OnValidationError>
+                                $('divAddNoKK').addClassName('has-error');
+                                jQuery('#<%=$this->FieldAddNoKKValidator->ClientID%>').removeAttr('style');
+                            </prop:ClientSide.OnValidationError>
+                            <prop:ClientSide.OnValidationSuccess>
+                                $('divAddNoKK').removeClassName('has-error');
+                            </prop:ClientSide.OnValidationSuccess>
+                        </com:TRequiredFieldValidator>
+                    </div>
+                </div>  
