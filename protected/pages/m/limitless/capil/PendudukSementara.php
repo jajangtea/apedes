17df9f7b8d28a8d87c1fa01854309f56f8b3570c
jjcollection <za2ng2509@hotmail.com>
2017-10-24 10:08:25 +0700

    tambah edit proses Induk Penduduk

---
+++ protected/pages/m/limitless/capil/PendudukSementara.php
@@ -0,0 +1,8 @@
+<?php
+prado::using ('Application.pagecontroller.m.capil.CPendudukSementara');
+class PendudukSementara extends CPendudukSementara {    
+    public function onLoad($param) {		
+        parent::onLoad($param);		                                   
+    }
+}
+?>
---
+++ protected/pages/m/limitless/capil/TambahPendudukSementara.php
@@ -0,0 +1,8 @@
+<?php
+prado::using ('Application.pagecontroller.m.capil.CTambahPendudukSementara');
+class TambahPendudukSementara extends CTambahPendudukSementara {    
+    public function onLoad($param) {		
+        parent::onLoad($param);		                                   
+    }
+}
+?>
