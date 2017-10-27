382675299f4322400ff7d15c34e44d64f9bf9030
jjcollection <za2ng2509@hotmail.com>
2017-10-28 05:54:26 +0700

    Rekap Penduduk

---
+++ protected/pages/m/limitless/capil/RekapJumlahPenduduk.php
@@ -0,0 +1,8 @@
+<?php
+prado::using ('Application.pagecontroller.m.capil.CRekapJumlahPenduduk');
+class RekapJumlahPenduduk extends CRekapJumlahPenduduk {    
+    public function onLoad($param) {		
+        parent::onLoad($param);		                                   
+    }
+}
+?>
